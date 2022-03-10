6d1113cfae1d65bc1b38faf1d8e23aba1e894b0a
gcerv93 <gcerv93@protonmail.com>
2022-03-10 10:41:09 -0600

    add processData function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import "./styles.css";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
+function processData(obj) {
+  const { temp } = obj.main;
+  const { humidity } = obj.main;
+  const wind = { windspeed: obj.wind.speed, windDegrees: obj.wind.deg };
+  const weather = {
+    id: obj.weather[0].id,
+    main: obj.weather[0].main,
+    description: obj.weather[0].description,
+  };
+
+  return { temp, humidity, wind, weather };
+}
+
 async function getLocationInfo(location) {
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=89dc410fda394c8340d84dbbcc93ee59`,
@@ -10,5 +23,8 @@ async function getLocationInfo(location) {
 
   response.json().then((res) => {
     console.log(res);
+    console.log(processData(res));
   });
 }
+
+getLocationInfo("Houston");
