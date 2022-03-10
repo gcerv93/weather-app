819c5efe31106189ec0e3c431df18d8c9814840d
gcerv93 <gcerv93@protonmail.com>
2022-03-09 22:21:20 -0600

    add getLocationInfo function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+async function getLocationInfo(location) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=89dc410fda394c8340d84dbbcc93ee59`,
+    { mode: "cors" }
+  );
+
+  response.json().then((res) => {
+    console.log(res);
+  });
+}
